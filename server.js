9fc090060f26b11adea230bc041293e4b58e95a9
Santiago <Santiagodiazboutet@gmail.com>
2020-08-27 20:35:18 -0300

    Tercer commit. Modificaciones para deploy en heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/angularapp));
+app.use(express.static(__dirname + '/angularapp'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
 res.sendFile(path.join(__dirname + '/angularapp/index.html'));
